8d6a6348f52d3945dab975b0ec222614ffc83abf
Christophe LC <christophelecoent@gmail.com>
2025-10-28 22:41:26 +0000

    Adjust vertical padding for radar category labels

--- src/components/d3-radar/RadarChart.tsx
+++ src/components/d3-radar/RadarChart.tsx
@@ -65,7 +65,7 @@ const RadarChart: React.FC<{ items: any[], radius: number, onEditClick: (item: a
 
     const drawCategoryLabels = (g: d3.Selection<SVGGElement, unknown, null, undefined>, radius: number) => {
         const offset = radius * 1.05; // Place labels just outside the main circle
-        const verticalPadding = 10;
+        const verticalPadding = 80;
     
         Object.values(radarConfig.categories).forEach(cat => {
             const angle = ((Math.PI / 2) * cat.quadrantIndex) + (Math.PI / 4); // Center angle of the quadrant
